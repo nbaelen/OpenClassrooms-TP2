be39a8c13eaaddf1ed3da189f0dd8e1ef50e214a
nbaelen <n.baelen@gmail.com>
2018-05-10 01:38:46 +0200

    Redéfintion de la mathode recevoirDegats pour prendre en compte l'atout sur le Guerrier

--- Guerrier.php
+++ Guerrier.php
@@ -2,4 +2,17 @@
 
 class Guerrier extends Personnage {
 
+        /* Méthodes d'instances */
+
+    /* Redéfinition de la méthode recevoirDegats pour prendre en compte l'atout */
+    public function recevoirDegats($pDegats) {
+        $this->calculerAtout();
+        $this->degats += $pDegats - $this->atout;
+        if ($this->degats >= 100) {
+            return self::PERSONNAGE_TUE;
+        } else {
+            return self::PERSONNAGE_BLESSE;
+        }
+    }
+
 }
