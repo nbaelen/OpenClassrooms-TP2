a5a8d5aa1f9a27de546631bd5346e47341f301d1
nbaelen <n.baelen@gmail.com>
2018-05-10 03:04:15 +0200

    Ajout d'une nouvelle classe Brute

---
+++ Brute.php
@@ -0,0 +1,16 @@
+<?php
+
+class Brute extends Personnage {
+
+        /* Méthodes d'instance */
+
+    /* Redéfinition de la méthode frapper() pour prendre en compte l'atout */
+    public function frapper(Personnage $pCible,$pDegats) {
+        if ($this === $pCible) {
+            return self::CEST_MOI;
+        } else {
+            $degats = $pDegats + $this->atout;
+            return $pCible->recevoirDegats((int) $pDegats);
+        }
+    }
+}
